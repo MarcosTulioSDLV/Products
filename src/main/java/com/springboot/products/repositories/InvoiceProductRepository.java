5570d63f95b4dc4f6c10da51c0c2c97ab4f0e049
MarcosTulioSDLV <mtsotodelavega@gmail.com>
2024-10-02 15:28:32 -0500

    updated interface-InvoiceProductRepository with InvoiceProductPK (it was with Long)

--- src/main/java/com/springboot/products/repositories/InvoiceProductRepository.java
+++ src/main/java/com/springboot/products/repositories/InvoiceProductRepository.java
@@ -1,10 +1,11 @@
 package com.springboot.products.repositories;
 
 import com.springboot.products.models.InvoiceProduct;
+import com.springboot.products.models.InvoiceProductPK;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct,Long> {
+public interface InvoiceProductRepository extends JpaRepository<InvoiceProduct,InvoiceProductPK> {
 
 }
